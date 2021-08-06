9457c4f6aa4a6d58264e34ca8dcddb095445b163
Christian Peterson <cdpeterson@email.arizona.edu>
2021-08-06 10:41:20 -0700

    make header elements sequential

--- pages/index.js
+++ pages/index.js
@@ -27,10 +27,10 @@ export default function Home({ posts }) {
 				<Frame images={images} />
 				<div>
 					<h1>Christian Peterson</h1>
-					<h3>Software Engineer</h3>
+					<h2>Software Engineer</h2>
 				</div>
 			</div>
-			<div className="links">
+			<div className="links-container">
 				<a href="https://www.linkedin.com/in/ChristianUA/">LinkedIn</a>
 				<a href="https://github.com/ChristianUA">Github</a>
 				<a href="https://christian-blog.cdn.prismic.io/christian-blog/457d41f6-846f-4bce-8b73-12b7d9fd9b71_Christian+Peterson+-+Resume.pdf">
@@ -66,7 +66,10 @@ export default function Home({ posts }) {
 						/>
 					))}
 			</div>
-			<Link href="/posts">See all posts</Link>
+			<div className="links-container">
+				<Link href="/posts">See all posts</Link>
+			</div>
+
 			<h2>projects</h2>
 			<div className="card-container">
 				{projects &&
