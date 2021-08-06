1dbbd218d6265bf7fefa53f99ad9d4edefe47ad0
Christian Peterson <cdpeterson@email.arizona.edu>
2021-08-06 11:13:11 -0700

    add custom favicons

--- pages/index.js
+++ pages/index.js
@@ -46,8 +46,24 @@ export default function Home({ posts }) {
 					name="viewport"
 					content="initial-scale=1.0, width=device-width"
 				/>
-				<meta charset="UTF-8" />
-				<link rel="icon" href="/favicon.ico" />
+				<meta charSet="UTF-8" />
+				<link
+					rel="apple-touch-icon"
+					sizes="180x180"
+					href="/apple-touch-icon.png"
+				/>
+				<link
+					rel="icon"
+					type="image/png"
+					sizes="32x32"
+					href="/favicon-32x32.png"
+				/>
+				<link
+					rel="icon"
+					type="image/png"
+					sizes="16x16"
+					href="/favicon-16x16.png"
+				/>
 			</Head>
 
 			<div className="header">
